820a1ab7e6febe1c5ab2c5bb6825b74c0d7bcd11
panchuwu <francisco.nunez.d@usach.cl>
2021-07-21 16:18:04 -0400

    style: to show workflow

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          This line of code will change the workflow in git
+          This line of code will change the workflow in git 
         </p>
         <a
           className="App-link"
