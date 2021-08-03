b0dcf2a181fe478e7ea16af28518a6d407bc0ff6
Benjamin Belmar <benjamin.belmar@usach.cl>
2021-08-02 23:46:36 -0400

    feat:multipage and diplomas view testing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
+import React from 'react';
 import './assets/css/App.css';
 import SendDocuments from './components/SendDocuments.jsx'
 import NavBar from './components/NavBar.jsx' 
+import Diplomas from './components/Diplomas.jsx'
+import { BrowserRouter as Router,Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <section className="components">
-        <NavBar></NavBar>
-        <SendDocuments />
-      </section>
-    </div>
+    <Router>
+      <NavBar/>
+        <Route exact path = "/" render = {() => {
+          return(
+            <div className="App">
+              <section className="components">
+                
+                <SendDocuments />
+              </section>
+            </div> 
+          )  
+        }}>
+        </Route> 
+        <Route exact path="/diplomas" render = {() => {
+          return(
+            <div className="App">
+              <section className="components">
+                
+                <Diplomas />
+              </section>
+            </div> 
+          )  
+        }}>
+        </Route>
+      
+    </Router> 
   );
 }
 
