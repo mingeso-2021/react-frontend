41c571d26362fe2191d4e53936b19246e0ae6dda
panchuwu <francisco.nunez.d@usach.cl>
2021-08-05 17:14:53 -0400

    fix: import swal components in "DescargarArchivos"

--- src/components/DescargarArchivos.js
+++ src/components/DescargarArchivos.js
@@ -1,3 +1,6 @@
+
+import swal from 'sweetalert';
+// Styles
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../assets/css/DescargarArchivos.css'
 
@@ -12,21 +15,20 @@ const index = {
     diploma: ''
 }
 
-
-
-    const res = [{
-        "id": "1",
-        "name": "Nombre1",
-        "mail": "nombre1@mail.com",
-        "diploma": ["Diploma 1" , "-Diploma 11"]
-      },
-      {
-        "id": "2",
-        "name": "Nombre2",
-        "mail": "nombre2@mail.com",
-        "diploma": ["Diploma 2" , "-Diploma 22"]
-      },
-    ]
+// Example data
+const res = [{
+    "id": "1",
+    "name": "Nombre1",
+    "mail": "nombre1@mail.com",
+    "diploma": ["Diploma 1" , "-Diploma 11"]
+  },
+  {
+    "id": "2",
+    "name": "Nombre2",
+    "mail": "nombre2@mail.com",
+    "diploma": ["Diploma 2" , "-Diploma 22"]
+  },
+]
 
 const  descargarArchivos = () =>{
     swal({
@@ -36,58 +38,57 @@ const  descargarArchivos = () =>{
 }
 
 const Archivos = (props) => {
-    const {} = props;
-    return (
-      <div>
-        <table className="table table-bordered table-hover">
-        <TableHeader></TableHeader>
-        
-        <TableBody></TableBody>
-        
-        </table>
-      </div>
-    );
-  }
+  const {} = props;
+  return (
+    <div>
+      <table className="table table-bordered table-hover">
+      <TableHeader></TableHeader>
+      
+      <TableBody></TableBody>
+      
+      </table>
+    </div>
+  );
+}
   
   
-  const TableHeader = () => {
-    
+const TableHeader = () => {
+  
+  return(
+    <thead className="thead-dark" key="header-1">
+        <tr key="header-0">
+          <th>Nombre</th>
+          <th>Mail</th>
+          <th>Diplomas</th>
+          <th>Archivos</th>
+        </tr>
+    </thead>
+  );
+}
+  
+  
+const TableBody = () => {
     return(
-      <thead className="thead-dark" key="header-1">
-          <tr key="header-0">
-            <th>Nombre</th>
-            <th>Mail</th>
-            <th>Diplomas</th>
-            <th>Archivos</th>
-          </tr>
-      </thead>
-    );
-  }
-  
-  
-  const TableBody = () => {
-      return(
-          res.map((value,index) =>{
-              return(
-                  <tr >
-                      <td>
-                          {value.name}
-                      </td>
-                      <td>
-                          {value.mail}
-                      </td>
-                      <td>
-                          {value.diploma}
-                      </td>
-                      <td>
-                        <button type="button" class="btn btn-success" onClick = {()=>descargarArchivos()}>Descargar</button>
-                       
-                      </td>
-                  </tr>
-              )
-          })
+        res.map((value,index) =>{
+            return(
+                <tr >
+                    <td>
+                        {value.name}
+                    </td>
+                    <td>
+                        {value.mail}
+                    </td>
+                    <td>
+                        {value.diploma}
+                    </td>
+                    <td>
+                      <button type="button" class="btn btn-success" onClick = {()=>descargarArchivos()}>Descargar</button>
+                    </td>
+                </tr>
+            )
+        })
 
-      );
-  }
+    );
+}
   
-  export default Archivos;
+export default Archivos;
