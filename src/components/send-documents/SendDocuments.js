e77d56c3fee0eca1df9983a35b9872efdffa8584
Ignacio Fernández <ignacio.fernandez.m@usach.cl>
2021-08-05 20:56:42 -0400

    feat: send documents

--- src/components/send-documents/SendDocuments.js
+++ src/components/send-documents/SendDocuments.js
@@ -1,8 +1,8 @@
 // native imports
-import { React, Component } from 'react';
+import { React } from 'react';
 // npm modules
-import axios from 'axios';
-import Dropzone  from 'react-dropzone';
+import 'react-dropzone-uploader/dist/styles.css'
+import Dropzone from 'react-dropzone-uploader'
 // style
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../assets/css/SendDocuments.css';
@@ -10,86 +10,48 @@ import '../../assets/css/SendDocuments.css';
 import logo_usach from '../../assets/images/logo_usach.png';
 
 
-class SendDocuments extends Component {
+// define backend destination url 
+const baseUrl = process.env.REACT_APP_BASE_URL
 
-  constructor() {
-    super();
-    this.baseUrl = process.env.REACT_APP_BASE_URL;
-    this.state = {
-      title:'',
-      content:'',
-      uploadfile:'',
-    };
-  }
-  // send data
-  handleSubmit = e => {
-    e.preventDefault();
-    console.log( e );
-    
-    axios.post(this.baseUrl + "/upload/file", {
-        // Insertar peticion
-    })
-    .then( response => {
-        // Aqui la respuesta (si es necesaria)
-    })
-    .catch( err => console.log(err) );
-  };
+const SendDocuments = () => {
 
-  // Dropzone module 
-  onDrop = acceptedFiles => {
-    if (acceptedFiles.length > 0) {
-      this.setState({ uploadfile: acceptedFiles[0] })
-    }
+  // URL Destination
+  const getUploadParams = ({ meta }) => { return { url: baseUrl + "/uploadfiles/upload/" } }
+
+  // 
+  const handleChangeStatus = ({ meta }, status) => {
+    console.log(status, meta)
   }
 
+  // 
+  const handleSubmit = (files, allFiles) => {
+    console.log(files.map(f => f.meta))
+    allFiles.forEach(f => f.remove())
+  } 
 
-  render() {
-
-    const maxSize = 3 * 1024 * 1024;
-
-    return (
-      <div className="container position-form form-size shadow p-3 mb-5 bg-white rounded"> 
-        <div className="row justify-content-center">
-          <img
-            alt=""
-            src={logo_usach}
-            className="img-box-logo"
-          />
-        </div>
-        <div className="row justify-content-center">
-          <label for="formFileMultiple" className="form-label">Seleccione sus documentos</label>
-        </div>
-        <div className="row justify-content-center">
-          <Dropzone
-            onDrop = {this.onDrop}
-            accept = "application/pdf"
-            minSize={1}
-            maxSize={maxSize}
-          >
-          
-          {({ getRootProps, getInputProps }) => (
-            <div className="container">
-              <form onSubmit = { this.handleSubmit.bind(this) }>
-                  <div className="input-field">
-                      <button className="btn pink lighten-1 z-depth-0">Create</button>
-                  </div>
-              </form>
-              <div {...getRootProps()}>
-                  <input {...getInputProps()} />
-                  {console.log("SelectedFile"+ {...getRootProps() })}
-                  <p>Choose image File</p>
-                  {this.state.uploadfile ? <p>Selected file: {this.state.uploadfile.name}</p> : null}
-              </div>
-            </div>
-        )}</Dropzone>
-
-        </div>
-        {/* <div className="row justify-content-center">
-          <button className="btn btn-size text-center" onClick = {this.handleSubmit.bind(this)}>Enviar</button>
-        </div> */}
-      </div>  
-    )
-  }
+  return (
+    <div className="container position-form form-size shadow p-3 mb-5 bg-white rounded"> 
+      <div className="row justify-content-center">
+        <img
+          alt=""
+          src={logo_usach}
+          className="img-box-logo"
+        />
+      </div>
+      <div className="row justify-content-center">
+        <label className="text-size">Seleccione sus documentos</label>
+      </div>
+      <div className="row justify-content-center">
+        <Dropzone
+        accept="application/pdf"
+        getUploadParams={getUploadParams}
+        onChangeStatus={handleChangeStatus}
+        onSubmit={handleSubmit}
+        styles={{ dropzone: { minHeight: 200, maxHeight: 250, width: 900} }}
+      />
+      </div>
+    </div>
+  )
 }
 
   
