fa82731c6227bf0cea02ed2df5cd958dc9b57407
Felipe <felipe.morales.sa@usach.cl>
2021-08-09 21:48:09 -0400

    feat: vista diplomas

--- src/components/Diplomas.js
+++ src/components/Diplomas.js
@@ -1,24 +1,67 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../assets/css/Diplomas.css'
 
-
+const baseUrl = process.env.REACT_APP_BASE_URL
 const Diplomas = () => {
-  return (
-    <div className="container-main">
-      <div className="card">
-        <div className="card-header">
-          <p>Featured</p>
+    const [postulations,setPostulations] = useState()
+    const [evaluators,setEvaluators] = useState()
+    const [diplomas,setDiplomas] = useState()
+
+    const fetchDiploma = async () =>{
+        const response = await axios.get(baseUrl + "/postulations/getall");
+        const responseEvaluators = await axios.get(baseUrl + "/evaluators/getall");
+        const responseDiplomas = await axios.get(baseUrl + "/diplomas/getall" );
+        setPostulations(response.data)
+        setEvaluators(responseEvaluators.data)
+        setDiplomas(responseDiplomas.data)
+    }
+    useEffect(()=>{
+        fetchDiploma()
+    },[])
+
+    const getData=(data,id)=>{
+        for (let value of data) {
+            if(value.id === id){
+                return value
+            }
+        }
+    }
+    
+
+    return (
+        <div>
+            <table className="table table-bordered table-hover">
+                <thead className="thead-dark">
+                    <tr>
+                        <th>Diplomado</th>
+                        <th>Profesor</th>
+                        <th>Contacto profesor</th>
+                    </tr>
+                    {!postulations?'':
+                        postulations.map((value,index) =>{
+                            return(
+                                <tr>
+                                    <td key ="name">
+                                        {!diplomas? '': getData(diplomas,value.id_diploma).name}
+                                    </td>
+                                    <td key = "mail">
+                                        {!evaluators? '': getData(evaluators,value.id_evaluator).name}
+                                    </td>
+                                    <td key = "diploma">
+                                        {!evaluators? '': getData(evaluators,value.id_evaluator).email}
+                                    </td>
+
+                                </tr>
+                            )
+                        })
+                    }
+                </thead>
+            </table>
         </div>
-        <div className="card-body">
-          <h5 class="card-title">Special title treatment</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="/" class="btn btn-primary">Go somewhere</a>
-        </div>
-        <button className="btn btn-primary">Go somewhere</button>
-      </div>
-    </div>          
-  )
+    )
 }
 
 export default  Diplomas;
